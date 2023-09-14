ba6bb7866cf0b21beae1bdfb0da1a2ba44628653
ChriRy <christensen.ryan0322@outlook.com>
2023-09-13 18:38:49 -0600

    Completed prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,96 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            string letterGrade = "A";
+
+            int sign = grade % 10;
+            if (sign < 3)
+            {
+                string gradeSign = "-";
+                Console.WriteLine($"Your grade is {letterGrade}{gradeSign}.");
+            }
+            else
+            {
+                Console.WriteLine($"Your grade is {letterGrade}.");
+            }
+
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            string letterGrade = "B";
+                        int sign = grade % 10;
+            if (sign < 3)
+            {
+                string gradeSign = "-";
+                Console.WriteLine($"Your grade is {letterGrade}{gradeSign}.");
+            }
+            else if (sign >= 7)
+            {
+                string gradeSign = "+";
+                Console.WriteLine($"Your grade is {letterGrade}{gradeSign}.");
+            }
+            else
+            {
+                Console.WriteLine($"Your grade is {letterGrade}.");
+            }
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            string letterGrade = "C";
+                        int sign = grade % 10;
+            if (sign < 3)
+            {
+                string gradeSign = "-";
+                Console.WriteLine($"Your grade is {letterGrade}{gradeSign}.");
+            }
+            else if (sign >= 7)
+            {
+                string gradeSign = "+";
+                Console.WriteLine($"Your grade is {letterGrade}{gradeSign}.");
+            }
+            else
+            {
+                Console.WriteLine($"Your grade is {letterGrade}.");
+            }
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            string letterGrade = "D";
+                        int sign = grade % 10;
+            if (sign < 3)
+            {
+                string gradeSign = "-";
+                Console.WriteLine($"Your grade is {letterGrade}{gradeSign}.");
+            }
+            else if (sign >= 7)
+            {
+                string gradeSign = "+";
+                Console.WriteLine($"Your grade is {letterGrade}{gradeSign}.");
+            }
+            else
+            {
+                Console.WriteLine($"Your grade is {letterGrade}.");
+            }
+        }
+        else 
+        {
+            string letterGrade = "F";
+            Console.WriteLine($"Your grade is {letterGrade}.");
+
+        }
+
+        if (grade > 70)
+        {
+            Console.WriteLine("Good job, you passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Better luck next time. ");
+        }
     }
 }
