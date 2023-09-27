08c23c5ac109ca87a676c75f1b3cbdb651544ce7
ChriRy <christensen.ryan0322@outlook.com>
2023-09-27 10:01:09 -0600

    tourney coding added

---
+++ inClass/tourneys/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Players
+Player joanna = new Player("Joanna Mother", 21);
+Player yoshi = new Player("Yoshi Fauter", 42);
+Player steve = new Player("Steve From Accounting", 84);
+Player meredith = new Player("Meredith from HR", 168);
+Player harambe = new Player("Harambe", 0);
+Player grumpy = new Player("Grumpy Cat", 00);
+Player gabe = new Player("Gabe Dog", 000);
+
+// Teams
+Team rents = new Team("'Rents");
+rents.AddPlayer(joanna);
+rents.AddPlayer(yoshi);
+
+rents.DisplayRoster();
+
+Team corporate = new Team("Corporate");
+corporate.AddPlayer(steve);
+corporate.AddPlayer(meredith);
+
+corporate.DisplayRoster();
+
+Team revivals = new Team("Revivals");
+revivals.AddPlayer(harambe);
+revivals.AddPlayer(grumpy);
+revivals.AddPlayer(gabe);
+
+revivals.DisplayRoster();
+
+
