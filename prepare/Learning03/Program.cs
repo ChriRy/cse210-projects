991ac0dab64630f6094e8b78e90220dfed7a0bdc
ChriRy <christensen.ryan0322@outlook.com>
2023-10-10 11:02:13 -0600

    learning 3 complete

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction one = new Fraction();
+        Fraction whole = new Fraction(3);
+        Fraction fraction = new Fraction(1,4);
+
+        Console.WriteLine(one.GetFractionString());
+        whole.SetTop(5);
+        Console.WriteLine(whole.GetFractionString());
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
