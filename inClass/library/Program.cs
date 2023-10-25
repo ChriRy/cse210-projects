8cc450f6589b9dc49beffbf7733bcf7c2457176a
ChriRy <christensen.ryan0322@outlook.com>
2023-10-24 19:54:24 -0600

    inheritance prep assignment

---
+++ inClass/library/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Loanable console = new Loanable();
+
+Dvd polar_express = new Dvd("The Polar Express");
+Book fringe = new Book("Journey to the Fringe", "314159", 314159);
+
+polar_express.CheckOut();
+polar_express.Display();
+
+
+fringe.CheckOut();
+fringe.ShowBookDetails();
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+
+        Assignment assignment = new Assignment("Ryan Christensen", "Programming in C#");
+        assignment.GetSummary();
+
+        MathAssignment calculus = new MathAssignment("Travis Erickson", "Calculus", "7.3", "1-4, 18-26");
+        calculus.GetSummary();
+        calculus.GetHomeworkList();
+
+        WritingAssignment biology = new WritingAssignment("Adam Christensen", "Biology", "The Powerhouse of the Cell");
+        biology.GetSummary();
+        biology.GetWritingInformation();
     }
 }
