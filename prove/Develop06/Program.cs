fb1c3ef80452b32c5960e25f9d8c66c2d6b52990
ChriRy <christensen.ryan0322@outlook.com>
2023-11-16 11:22:52 -0700

    renamed some files and uploaded various in class projects

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -1,34 +1,9 @@
-﻿bool loop = true;
+using System;
 
-while (loop)
+class Program
 {
-    Console.WriteLine("Menu Options: ");
-    Console.WriteLine(" 1. Start Breathing Activity");
-    Console.WriteLine(" 2. Start Reflecting Activity");
-    Console.WriteLine(" 3. Start Listing Activity");
-    Console.WriteLine(" 4. Quit");
-
-    Console.WriteLine("Select a choice from the menu: ");
-    var choice = Console.ReadLine();
-    int i = Convert.ToInt16(choice);
-
-    switch (i)
+    static void Main(string[] args)
     {
-        case 1:
-            Breathing activity1 = new Breathing();
-            activity1.BreatheActivity();
-            break;
-        case 2:
-            Reflecting activity2 = new Reflecting();
-            activity2.ReflectActivity();
-            break;
-        case 3:
-            Listing activity3 = new Listing();
-            activity3.ListActivity();
-            break;
-        case 4:
-            Console.WriteLine("Have a nice day!");
-            loop = false;
-            break;
+        Console.WriteLine("Hello Develop05 World!");
     }
 }
