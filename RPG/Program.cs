f152e9b7986c8456bb300b6f9e9f71f221e5d032
ChriRy <131827854+ChriRy@users.noreply.github.com>
2024-09-20 17:30:04 -0600

    hello world

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -12,35 +12,35 @@ party.AddItems(itemList);
 
 Shop store = new Shop(party);
 
-List<Enemy> enemies = new List<Enemy>() { new Boredom(), new Anxiety(), new Loneliness() };
+List<Enemy> enemies = new List<Enemy>() { new Boredom("Monster 1"), new Anxiety("Monster 2"), new Loneliness("Monster 3") };
 
-Battle round = new Battle(party, enemies);
-round.BattleLoop();
+// Battle round = new Battle(party, enemies);
+// round.BattleLoop();
 
 
 
-// Cutscene intro = new Cutscene("textfiles/intro.txt");
-// Console.Clear();
+Cutscene intro = new Cutscene("textfiles/intro.txt");
+Console.Clear();
 
-// Console.WriteLine("Hero, what is your name? ");
-// string name = Console.ReadLine()!;
-// Medication hero = new Medication(name);
-// Team party = new Team(hero);
-// Console.Clear();
+Console.WriteLine("Hero, what is your name? ");
+string name = Console.ReadLine()!;
+Medication hero = new Medication(name);
+party = new Team(hero);
+Console.Clear();
 
-// Cutscene tutorial = new Cutscene("textfiles/tutorial.txt");
+Cutscene tutorial = new Cutscene("textfiles/tutorial.txt");
 
-// List<Enemy> enemies1 = new List<Enemy>() { new Boredom() };
-// Battle round1 = new Battle(party, enemies1);
+List<Enemy> enemies1 = new List<Enemy>() { new Boredom() };
+Battle round1 = new Battle(party, enemies1);
 
-// Cutscene scene1 = new Cutscene("textfiles/scene1.txt");
-// Counselor thera = new Counselor("Thera");
-// party.AddPartyMember(thera);
-// List<Enemy> enemies2 = new List<Enemy>() { new Boredom(), new Anxiety(), new Loneliness() };
-// Battle round2 = new Battle(party, enemies2);
+Cutscene scene1 = new Cutscene("textfiles/scene1.txt");
+Counselor thera = new Counselor("Thera");
+party.AddPartyMember(thera);
+List<Enemy> enemies2 = new List<Enemy>() { new Boredom(), new Anxiety(), new Loneliness() };
+Battle round2 = new Battle(party, enemies2);
 
-// Cutscene scene2 = new Cutscene("textfiles/scene2.txt");
-// Shop shop = new Shop(party);
+Cutscene scene2 = new Cutscene("textfiles/scene2.txt");
+Shop shop = new Shop(party);
 
-// Cutscene scene3 = new Cutscene("textfiles/scene3.txt");
+Cutscene scene3 = new Cutscene("textfiles/scene3.txt");
 
